fbf8a4188370ef626d2a8382bcc43e47d653ccd9
Errol Cleary <errol.cleary@gmail.com>
2020-12-05 09:50:37 -0800

    Add notes for planning & organization

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -45,28 +45,32 @@ var App = {
 //   roomname: 'glorious room'
 // };
 
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.sfe.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'http://parse.sfe.hackreactor.com/chatterbox/classes/messages',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });
 
-$.ajax({
-  url: Parse.server,
-  type: 'GET',
-  data: { order: '-createdAt' },
-  contentType: 'application/json',
-  success: successCB,
-  error: errorCB || function(error) {
-    console.error('chatterbox: Failed to fetch messages', error);
-  }
-});
+// $.ajax({
+//   url: Parse.server,
+//   type: 'GET',
+//   data: { order: '-createdAt' },
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: errorCB || function(error) {
+//     console.error('chatterbox: Failed to fetch messages', error);
+//   }
+// });
+
+// var errorCB;
--- spec/notes.md
+++ spec/notes.md
@@ -3,3 +3,26 @@
 [] Parse.create success cb and error cb will likely need to be customized
 [] renderMessage does not currently refer to message input param (messagesView.js)
 []
+
+### MODELS (all currently empty objects)
+* messages.js
+* rooms.js
+* friends.js
+​
+### VIEWS (note nesting hierarchy)
+html page
+ * formView
+ * roomsView
+   * messagesView
+     * messageView
+​
+### VIEWS (nesting hierarchy explicated)
+html page
+ * formView  (child of html page)
+ * roomsView (child of html page)
+   * messagesView (child of roomsView)
+     * messageView (child of messagesView)
+​
+### CONTROLLERS
+* app.js
+* parse.js
