01003152a4e79b60e5c95f20c8ad5b2f1f9c7152
Errol Cleary <errol.cleary@gmail.com>
2020-12-05 12:02:21 -0800

    Add functionality to message views

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -8,8 +8,15 @@ var MessagesView = {
   render: function() {
   },
 
+  testMessage: {
+    text: 'got you',
+    username: 'errol',
+    roomname: 'best room'
+  },
+
   renderMessage: function() {
-    MessagesView.$chats.append('<div></div>');
+    // MessagesView.$chats.append('<div></div>');
+    MessagesView.$chats.append(MessageView.render(MessagesView.testMessage));
   }
 
 };
